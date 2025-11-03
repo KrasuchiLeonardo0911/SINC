e39ac0ac9fd476ec97e7cd3b286519d9f23ad6d8
svleo1 <Krasuchileonardo@gmail.com>
2025-11-03 17:54:38 -0300

    prueba de subida de movimientos al servidor local pasada

--- data/src/main/java/com/sinc/mobile/data/network/api/MovimientoApiService.kt
+++ data/src/main/java/com/sinc/mobile/data/network/api/MovimientoApiService.kt
@@ -4,11 +4,9 @@ import com.sinc.mobile.data.network.dto.MovimientosBatchRequest
 import okhttp3.ResponseBody
 import retrofit2.Response
 import retrofit2.http.Body
-import retrofit2.http.Headers
 import retrofit2.http.POST
 
 interface MovimientoApiService {
-    @Headers("Accept: application/json")
-    @POST("movil/cuaderno/movimientos")
+    @POST("api/movil/cuaderno/movimientos")
     suspend fun saveMovimientos(@Body request: MovimientosBatchRequest): Response<ResponseBody>
 }
